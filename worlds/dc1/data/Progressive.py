060bdea720ea25100ab535608f06b555db7c2a74
Lizardman175 <lizardman175@gmail.com>
2026-04-16 15:25:43 -0600

    Created new file for progressive char recruitment buildings

---
+++ worlds/dc1/data/Progressive.py
@@ -0,0 +1,69 @@
+from worlds.dc1.JunkDrawer import progressive_char_recruit_name
+
+all_chars = {
+    "Player's House": progressive_char_recruit_name,
+    "Player's Keg": progressive_char_recruit_name,
+    "Player's Storage": progressive_char_recruit_name,
+    "Renee": progressive_char_recruit_name,
+    "Player's Llama": progressive_char_recruit_name,
+    "Player's Chimney": progressive_char_recruit_name,
+    "Stray Cat":  progressive_char_recruit_name,
+
+    "Cacao's House": progressive_char_recruit_name,
+    "Cacao's Stairway": progressive_char_recruit_name,
+    "Cacao's Cabin": progressive_char_recruit_name,
+    "Cacao": progressive_char_recruit_name,
+    "Cacao's Torch": progressive_char_recruit_name,
+    "Cacao's Sign": progressive_char_recruit_name,
+    "Cacao's Laundry": progressive_char_recruit_name,
+
+    "King's Hideout": progressive_char_recruit_name,
+    "King's Sunshade": progressive_char_recruit_name,
+    "King's Car": progressive_char_recruit_name,
+    "Jake": progressive_char_recruit_name,
+    "King": progressive_char_recruit_name,
+    "Stu": progressive_char_recruit_name,
+    "King's Lamp": progressive_char_recruit_name,
+
+    "3 Sisters' House": progressive_char_recruit_name,
+    "Nagita": progressive_char_recruit_name,
+    "Devia": progressive_char_recruit_name,
+    "Mikara": progressive_char_recruit_name,
+    "Sisters' Mini Totem Pole": progressive_char_recruit_name,
+    "Sisters' Cabin": progressive_char_recruit_name,
+    "Sisters' Odds & Ends": progressive_char_recruit_name,
+}
+
+split_chars = {
+    "Player's House": "Progressive Player's House",
+    "Player's Keg": "Progressive Player's House",
+    "Player's Storage": "Progressive Player's House",
+    "Renee": "Progressive Player's House",
+    "Player's Llama": "Progressive Player's House",
+    "Player's Chimney": "Progressive Player's House",
+    "Stray Cat": "Progressive Player's House",
+
+    "Cacao's House": "Progressive Cacao's House",
+    "Cacao's Stairway": "Progressive Cacao's House",
+    "Cacao's Cabin": "Progressive Cacao's House",
+    "Cacao": "Progressive Cacao's House",
+    "Cacao's Torch": "Progressive Cacao's House",
+    "Cacao's Sign": "Progressive Cacao's House",
+    "Cacao's Laundry": "Progressive Cacao's House",
+
+    "King's Hideout": "Progressive King's Hideout",
+    "King's Sunshade": "Progressive King's Hideout",
+    "King's Car": "Progressive King's Hideout",
+    "Jake": "Progressive King's Hideout",
+    "King": "Progressive King's Hideout",
+    "Stu": "Progressive King's Hideout",
+    "King's Lamp": "Progressive King's Hideout",
+
+    "3 Sisters' House": "Progressive 3 Sisters' House",
+    "Nagita": "Progressive 3 Sisters' House",
+    "Devia": "Progressive 3 Sisters' House",
+    "Mikara": "Progressive 3 Sisters' House",
+    "Sisters' Mini Totem Pole": "Progressive 3 Sisters' House",
+    "Sisters' Cabin": "Progressive 3 Sisters' House",
+    "Sisters' Odds & Ends": "Progressive 3 Sisters' House",
+}
