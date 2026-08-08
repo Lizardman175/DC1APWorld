c3b2b204c16d7da7d38325ace155bc7d8af60c68
Lizardman175 <lizardman175@gmail.com>
2026-08-08 09:34:19 -0600

    Added comment to track item ID patterns

--- worlds/dc1/Items.py
+++ worlds/dc1/Items.py
@@ -3,6 +3,16 @@ import re
 from BaseClasses import Item, ItemClassification
 from .game_id import dc1_name
 
+# Item ID patterns: (all prefixed with 97111)
+# Georama     0Axx:  0 = Georama, A = town index, xx = georama piece
+# Consumables 1xxx:  1 = Consumable item category, xxx = item ID from game
+# Attachments 2xxx:  2 = Attachment item category, xxx = item ID from game
+# +x attach   20AB: 20 = Attachment, A = type 0-3 (att/speed/end/mag), B = +1/2/3
+# Weapons     3xxx:  3 = Weapon item category, xxx = item ID from game (unused)
+#
+# Special Item IDs:
+# Progressive Char Building 0000
+# Dark Genie flag           9999
 
 class DarkCloudItem(Item):
     game = dc1_name
